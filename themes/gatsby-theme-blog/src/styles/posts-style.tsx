95437d200c13ee9259d234646a8ef9857929a87a
hppRC <hpp.ricecake@gmail.com>
2020-02-22 13:26:50 +0900

    :sparkles: add shared posts collection page style

---
+++ themes/gatsby-theme-blog/src/styles/posts-style.tsx
@@ -0,0 +1,37 @@
+import { css } from '@emotion/core';
+
+export default css`
+  position: relative;
+
+  > section:nth-of-type(1) {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 75vh;
+
+    > h1 {
+      font-size: 10rem;
+      color: #ffffff;
+    }
+  }
+
+  > section:nth-of-type(2) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 2rem;
+    padding: 5rem;
+  }
+
+  @media screen and (max-width: 1100px) {
+  }
+  @media screen and (max-width: 768px) {
+  }
+  @media screen and (max-width: 480px) {
+  }
+  @media screen and (max-height: 430px) {
+  }
+`;
