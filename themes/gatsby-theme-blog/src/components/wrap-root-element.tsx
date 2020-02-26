795fd81456819d1a7175e72031bdaf4cff75b4d4
hppRC <hpp.ricecake@gmail.com>
2020-02-26 14:49:51 +0900

    🗑 remove emotion theming

--- themes/gatsby-theme-blog/src/components/wrap-root-element.tsx
+++ themes/gatsby-theme-blog/src/components/wrap-root-element.tsx
@@ -1,21 +1,9 @@
-import { ThemeProvider } from 'emotion-theming';
 import React from 'react';
 
 import { ColorModeContainer } from '../store';
 
-const theme = {
-  colors: {
-    darkCharacterColor: '#ffffff',
-    darkBackground: '#09090f',
-    lightCharacterColor: '#09090f',
-    lightBackground: '#ffffff'
-  }
-} as const;
-
 export const WrapRootElement = ({ element }: { element: React.FCX }) => (
-  <ThemeProvider theme={theme}>
-    <ColorModeContainer.Provider>{element}</ColorModeContainer.Provider>
-  </ThemeProvider>
+  <ColorModeContainer.Provider>{element}</ColorModeContainer.Provider>
 );
 
 export default WrapRootElement;
