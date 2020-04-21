70a40cddf3402e2297f68444faa4e10dc65c32bb
hppRC <hpp.ricecake@gmail.com>
2020-04-22 01:54:56 +0900

    :+1: add import for distributed css files

--- themes/gatsby-theme-blog/gatsby-ssr.js
+++ themes/gatsby-theme-blog/gatsby-ssr.js
@@ -1,7 +1,9 @@
 import 'prismjs/plugins/command-line/prism-command-line.css';
 import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import 'katex/dist/katex.min.css';
+import './src/styles/reset.scss';
 
 import { WrapRootElement, WrapPageElement } from './src/components';
+
 export const wrapRootElement = WrapRootElement;
 export const wrapPageElement = WrapPageElement;
